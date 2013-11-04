4a25b3e8506e3ca7738602376629aab0166596dc
Keir 'Kaffo' Smith <kaffse@gmail.com>
2013-11-04 13:06:53 +0000

    Webapp setup

--- mdrs/settings.py
+++ mdrs/settings.py
@@ -1,11 +1,17 @@
-# Django settings for mdrs project.
+import os
+PROJECT_PATH = os.getcwd()
+TEMPLATE_PATH = os.path.join(PROJECT_PATH, 'templates')
+STATIC_PATH = os.path.join(PROJECT_PATH, 'static')
+DATABASE_PATH = os.path.join(PROJECT_PATH, 'webapp.db')
 
+# Django settings for mdrs project.
 DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
     # ('Your Name', 'your_email@example.com'),
-	('Keir Smith', 'kaffse@gmail.com'),
+    ('Keir Smith', 'kaffse@gmail.com'),
+    ('Ally Weir', 'ally.pcgf@gmail.com'),
 )
 
 MANAGERS = ADMINS
@@ -13,7 +19,7 @@ MANAGERS = ADMINS
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
-        'NAME': 'mdrs',                      # Or path to database file if using sqlite3.
+        'NAME': 'mdrsDatabase',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
         'USER': 'mdrs',
         'PASSWORD': 't34mt34mt',
@@ -51,12 +57,12 @@ USE_TZ = True
 
 # Absolute filesystem path to the directory that will hold user-uploaded files.
 # Example: "/var/www/example.com/media/"
-MEDIA_ROOT = ''
+MEDIA_ROOT = os.path.join(PROJECT_PATH, 'media')
 
 # URL that handles the media served from MEDIA_ROOT. Make sure to use a
 # trailing slash.
 # Examples: "http://example.com/media/", "http://media.example.com/"
-MEDIA_URL = ''
+MEDIA_URL = '/media/'
 
 # Absolute path to the directory static files should be collected to.
 # Don't put anything in this directory yourself; store your static files
@@ -73,6 +79,7 @@ STATICFILES_DIRS = (
     # Put strings here, like "/home/html/static" or "C:/www/django/static".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    STATIC_PATH,
 )
 
 # List of finder classes that know how to find static files in
@@ -112,6 +119,7 @@ TEMPLATE_DIRS = (
     # Put strings here, like "/home/html/django_templates" or "C:/www/django/templates".
     # Always use forward slashes, even on Windows.
     # Don't forget to use absolute paths, not relative paths.
+    TEMPLATE_PATH,
 )
 
 INSTALLED_APPS = (
@@ -125,6 +133,7 @@ INSTALLED_APPS = (
     'django.contrib.admin',
     # Uncomment the next line to enable admin documentation:
     # 'django.contrib.admindocs',
+    'webapp',
 )
 
 SESSION_SERIALIZER = 'django.contrib.sessions.serializers.JSONSerializer'
--- webapp/views.py
+++ webapp/views.py
@@ -1,1 +1,11 @@
-# Create your views here.
+from django.http import HttpResponse
+from django.template import RequestContext
+from django.shortcuts import render_to_response
+
+def index(request):
+    context = RequestContext(request)
+    context_dict = {'boldmessage': "I am from context"}
+    return render_to_response('webapp/index.html', context_dict, context)
+
+def about(request):
+   return HttpResponse("<h3>This is MDRS' about page.</h3>")
