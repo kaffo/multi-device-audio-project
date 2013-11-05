0e4d3ce797c954d6333096212a79c26a0bd9e900
allyjweir <ally.pcgf@gmail.com>
2013-11-05 12:47:30 +0000

    added a comment

--- webapp/urls.py
+++ webapp/urls.py
@@ -1,6 +1,6 @@
 from django.conf.urls import patterns, url
 from webapp import views
-
+#comment to show that this syncing stuff is working. work local. commit to server
 urlpatterns = patterns ('',
     url(r'^$', views.index, name='index'),
     url(r'^$', views.submit, name='submit'),
