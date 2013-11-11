8920fe2bf4f51e3b33a9c7f5e1aa217f14345640
Keir Smith <kaffse@gmail.com>
2013-11-11 13:09:47 +0000

    Can now post text via form, next is posting files

--- webapp/views.py
+++ webapp/views.py
@@ -1,7 +1,7 @@
 from django.http import HttpResponse, HttpRequest
 from django.template import RequestContext
 from django.shortcuts import render_to_response
-from .forms import UploadFileForm
+from .form import UploadFileForm
 
 def index(request):
     context = RequestContext(request)
@@ -12,11 +12,13 @@ def about(request):
    return HttpResponse("<h3>This is MDRS' about page.</h3>")
 
 def submit(request):
+    context = RequestContext(request)
     if request.method == 'GET':
         form = UploadFileForm()
     if request.method == 'POST':
         form = UploadFileForm(request.POST, request.FILES)
+        print("Recieved File")
         if form.is_valid():
             return HttpResponse("<h1>Upload Success!</h1>")
-    return render_tp_response('submit.html', {'form': form})
+    return render_to_response('webapp/submit.html', {'form': form}, context)
 
