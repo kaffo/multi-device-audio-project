ef2e7a4839801e4cb76d6da4d32cdd16d329f406
gtadam <gtadam89@gmail.com>
2014-01-14 02:15:52 +0000

    Included submit in the website, I kept the original html in case I damaged something

--- webapp/urls.py
+++ webapp/urls.py
@@ -4,13 +4,14 @@ from webapp import views
 urlpatterns = patterns ('',
     url(r'^$', views.index, name='index'),
     url(r'^about/$', views.about, name='about'),
-    url(r'^submit/$', views.submit, name='submit'),
+    url(r'^submit', views.submit, name='submit'),
     url(r'^getdata:(-?[0-9]+.-?[0-9]+):(-?[0-9]+.-?[0-9]+):(-?[0-9]+.-?[0-9])+:(-?[0-9]+.-?[0-9]+)/$', views.getdata, name='getdata'),
     url(r'^settings', views.settings, name='settings'),
     url(r'^user', views.user, name='user'),                    
     #commented out, so it is not lost in case i'm wrong (Gordon Adam)
     #url(r'^settings/$', views.settings, name='settings'),
     #url(r'^user/$', views.user, name='user'),
+    #url(r'^submit/$', views.submit, name='submit'),
                         
     #url(r'^$', views.register, name='register'),
     #url(r'^$', views.user_login, name='login'),
