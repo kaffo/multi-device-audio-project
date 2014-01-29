2c1890e3c244d4e6e6d1fa20b4144b6f30fa5b69
Kaffse <keir2789@hotmail.com>
2014-01-29 20:14:47 +0000

    Added venv for this project, since bugger installing everything globably...

---
+++ venv/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/home/kaffo/workspace/lvl3project/multi-device-audio-project/venv/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
