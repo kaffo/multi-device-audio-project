f56d2afbf1dd74a31c46b46d56e98684beb6a8b9
gtadam <gtadam89@gmail.com>
2014-02-04 21:01:42 +0000

    Stopped you having to type /webapp after the url

--- mdrs/urls.py
+++ mdrs/urls.py
@@ -15,7 +15,7 @@ urlpatterns = patterns('',
 
     # Uncomment the next line to enable the admin:
     url(r'^admin/', include(admin.site.urls)),
-    url(r'^webapp/', include('webapp.urls')),
+    url(r'^', include('webapp.urls')),
 )
 
 #if settings.DEBUG:
