6658ff14981456e863b1af6449878addf4dfef68
Georgi Dimitrov <gogodi91@gmail.com>
2014-02-13 13:25:48 +0000

    play all sounds and stop them again

--- static/scripts/buzztest.js
+++ static/scripts/buzztest.js
@@ -1,4 +1,6 @@
 alert("This script is working");
+
+var curTime = new Date().getTime();
 var mySounds = new Array();
 var mySound1 = new buzz.sound( "../../static/data/test1.ogg", 
 	{	
@@ -12,28 +14,47 @@ var mySound3 = new buzz.sound("../../static/data/Second Audio Test.ogg",
 	{
 	preload: true, autoplay:false
 	});
-mySounds.push
+//populate an array with all sounds we want to play
+mySounds.push(mySound1);
+mySounds.push(mySound2);
+mySounds.push(mySound3);
 
+//load all sounds
 
+//for (var i=0;i<mySounds.length;i++)
+	//{
+	//mySounds[i].load();
+	//}
+//play all sounds to test they work
+for (var i=0;i<mySounds.length;i++)
+	{
+	mySounds[i].play();
+	}
+console.log(mySounds[0].getTime());
+
+for (var i=0;i<mySounds.length;i++)
+	{
+	mySounds[i].stop();
+	}
 ///////////////////// play factory ///////////////////
-mySound.unmute();
+//mySound.unmute();
 //mySound.setVolume(10);
-mySound.load();
+//mySound.load();
 //mySound.play();
 
-mySound.bind("timeupdate", function(e) {
+//mySound.bind("timeupdate", function(e) {
     //var percent = buzz.toPercent( this.getTime(), this.getDuration() ),
     //    message = "Stopped or paused at " + percent + "%";
     //document.getElementById("percent").innerHTML = message;
-    var seconds = mySound.getTime();
+//    var seconds = mySound.getTime();
     //console.log("this sux");
-    if (seconds>3)
-    	{mySound.stop();
+//    if (seconds>3)
+//    	{mySound.stop();
 		//mySound2.play();
     	//mySound2.stop();
-    	}
-    console.log(mySound.getTime());
-    });
+    	//}
+    //console.log(mySound.getTime());
+    //});
 
 ///////////////////// timer factory ////////////////////
 
