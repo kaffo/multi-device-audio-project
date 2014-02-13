0e79a2fc195218e6dd64b6a2862d08b6287dbeb5
Georgi Dimitrov <gogodi91@gmail.com>
2014-02-13 13:06:57 +0000

    add sounds to array

--- static/scripts/buzztest.js
+++ static/scripts/buzztest.js
@@ -1,19 +1,25 @@
 alert("This script is working");
-
-var mySound = new buzz.sound( "../../static/data/test1.ogg", 
+var mySounds = new Array();
+var mySound1 = new buzz.sound( "../../static/data/test1.ogg", 
 	{	
 	preload: true,autoplay: false
 	});
 var mySound2 = new buzz.sound("../../static/data/second_audio.ogg",
 	{
 	preload: true, autoplay:false
-	})
+	});
+var mySound3 = new buzz.sound("../../static/data/Second Audio Test.ogg",
+	{
+	preload: true, autoplay:false
+	});
+mySounds.push
+
 
 ///////////////////// play factory ///////////////////
 mySound.unmute();
 //mySound.setVolume(10);
 mySound.load();
-mySound.play();
+//mySound.play();
 
 mySound.bind("timeupdate", function(e) {
     //var percent = buzz.toPercent( this.getTime(), this.getDuration() ),
@@ -21,10 +27,11 @@ mySound.bind("timeupdate", function(e) {
     //document.getElementById("percent").innerHTML = message;
     var seconds = mySound.getTime();
     //console.log("this sux");
-    if (seconds>7 && confirm("stop?")===true && seconds )
+    if (seconds>3)
     	{mySound.stop();
-		mySound2.play();}
+		//mySound2.play();
     	//mySound2.stop();
+    	}
     console.log(mySound.getTime());
     });
 
