2522b4fa71078ad6adaa69bb77e4d8c349c6cfd8
ppyordanov <ppyordanov@yahoo.com>
2014-02-13 02:23:18 +0000

    cleaned the script and improved it, now works with a single play/pause button
    comments added and some minor bugs fixed; only bug now is that when a synchronised object finishes playing once tha toggle button is pressed to pause group it starts from the beginning with the other sounds paused which is logical but not what we need. Will fix this.

--- static/scripts/syncSound.js
+++ static/scripts/syncSound.js
@@ -1,6 +1,6 @@
-//pyordanov
+//pyordanov v3.0
 
-jQuery.ajaxSetup({async:false});
+//jQuery.ajaxSetup({async:false});
 
 var group;
 
@@ -14,7 +14,11 @@ var sync = new Array();
 var sync_group = new Array();
 
 var loaded = 0;
-			
+//recording information to be displayed to the user before playback
+var info = "Rec.: \t Sync at: \n";
+//var duration=2;
+
+//a function to sort the synchronisation array
 function compare(a,b){
 	if(a.fields.start_time<b.fields.start_time)
 		return -1;
@@ -23,6 +27,10 @@ function compare(a,b){
 	return 0;
 }
 
+/*initial data processing function filtering recordings
+that are to be synchronised depending on recording times
+(takes an array of recordings from the database as input parameter)
+*/
 function process_data(recs){
 	
 	var check_rec;
@@ -30,15 +38,14 @@ function process_data(recs){
 	var check_start;
 	var check_end;
 	
+	//populating the sync array with relevant recording objects
 	for(var i=0;i<recs.length;i++){
 		check_rec = recs[i];
 		check_id = check_rec.pk;
 		check_start = new Date(check_rec.fields.start_time);
 		check_end = new Date(check_rec.fields.end_time);
-		//alert(check_end.getTime());
-		//alert(curr_start <= check_start );
-		//alert(check_end<= curr_end);
-		//alert(check_start + " " + check_end);
+		
+		//start-end time comparison check
 		if((curr_start.getTime() <= check_start.getTime() && check_end.getTime()<= curr_end.getTime()) || 
 			(curr_start.getTime()>=check_start.getTime() && curr_start.getTime()<=check_end.getTime()) || 
 			(curr_end.getTime()>=check_start.getTime() && curr_end.getTime()<=check_end.getTime())
@@ -47,38 +54,37 @@ function process_data(recs){
 		{
 			sync.push(check_rec);
 		}
-		//alert(sync[1].fields.rec_file + "da");
-		//alert(curr_start + " " + curr_end + " " + check_start + " " + check_end );
+
 	}
 	
-	//alert(sync[1].fields.rec_file + "tosync");
-	
 	sync = sync.sort(compare);
-	//alert(sync.length);
 	return sync;
 	
 }
 
+/*using the array with relevant recording objects create buzz! sound objects,
+set their times accordingly, and populate the group array sync_group
+*/
 function load_data(sync){
 	var diff;
 	var last = sync[sync.length-1];
 	var s_obj;
-	//alert(last.fields.start_time);
 	
 	var l = new Date(last.fields.start_time);
 	var s;
 
 	for(var i=0;i<sync.length;i++){
 		s = new Date(sync[i].fields.start_time);
-		//alert("../../" + sync[i].fields.rec_file);
 		s_obj = new buzz.sound("../../" + sync[i].fields.rec_file);
 		sync_group.push(s_obj);
+		
+		//use start time difference to calibrate synchronisation
 		diff = (l.getTime() - s.getTime())/1000;
-		//alert(l + " " + l.getTime() + " " + s + " " + sync[i].fields.end_time + " " + s.getTime());
-		//diff = 10;
+		
+		info += sync[i].fields.rec_file.replace('static/data/', '') + "\t " + diff + "s \n";
+		
 		if(diff>=0){
 			s_obj.setTime(diff);
-			//alert(l.getTime() + " " + s.getTime());
 		}
 		
 		else{
@@ -86,13 +92,25 @@ function load_data(sync){
 		}
 		
 	}
-	//alert(sync_group.length);
-	loaded = 1;
 	
+	loaded = 1;
+	//display recording information before playback
+	alert(info);
 }
 
 function synchronise(id){
 
+	//check if html5 audio tag is supported
+	if (!buzz.isSupported()) {
+    alert("You need to update your browser to view this content");
+	}
+	
+	//check if .OGG audio format is supported
+	if (!buzz.isOGGSupported()) {
+    alert("Your browser does not support .ogg audio format.");
+	}
+
+	//get request for accessing current sound object attributes
 	$.get("/webapp/playSound:" + id,
 		
 		function(data){
@@ -100,35 +118,23 @@ function synchronise(id){
 			curr_start = new Date(curr_rec[0].fields.start_time);
 			curr_end= new Date(curr_rec[0].fields.end_time);
 			curr_s_obj = new buzz.sound("../../" + curr_rec[0].fields.rec_file);
-			//alert(curr_rec[0].fields.rec_file);
 		});
 		
 		
-
+	//get request for populating the recs array with database recording objects and process the data
 	$.get(
 		"/webapp/getRecs",
 		
 		function(data){
 			recs = eval("("+ data +")");
-			//alert(recs[0].fields.start_time);
 			var toSync = process_data(recs);
-			//alert(toSync.length);
+			
 			load_data(toSync);
-			//sync_group.push(curr_s_obj);
-			//alert("final" + sync_group.length);
 	
 			group = new buzz.group(sync_group);
-			//group.togglePlay();
 		});
 	
 
-	
-	//var s1 = new buzz.sound( "../../static/data/second_audio.ogg");
-	//var s = new buzz.sound( "../../static/data/" + id +".ogg"); //curr_rec.fields.rec_file
-	
-	//s.setTime(25.5);
-	//sync_group.push(s1);
-	//sync_group.push(s);
 }
 
 function load(id){
@@ -138,6 +144,24 @@ function load(id){
 	}
 }
 
-function playS(){
+function playS(id){
+
+	//alert("Loaded: " + loaded);
+	if(loaded==0){
+		synchronise(id);
+	}
+	
 	group.togglePlay();
+	
 }
+
+/*
+group.bind("timeupdate", function(e){
+	if(group.getTime()>duration){
+		group.stop();
+	}
+	alert("go");
+	console.log(group.getTime());
+});
+
+*/
