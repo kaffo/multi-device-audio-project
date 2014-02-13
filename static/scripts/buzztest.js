af01cd056e1c2e5fdfd0f123d1a3347a1e14782e
Georgi Dimitrov <gogodi91@gmail.com>
2014-02-13 14:25:39 +0000

    recursive function to call the recordings

--- static/scripts/buzztest.js
+++ static/scripts/buzztest.js
@@ -1,19 +1,23 @@
+//hardcoded solution to the buzz problem
 alert("This script is working");
 
-var curTime = new Date().getTime();
+var startTime = new Date().getTime();
 var mySounds = new Array();
+var current = 0;//number of file currently playing
+
 var mySound1 = new buzz.sound( "../../static/data/test1.ogg", 
 	{	
-	preload: true,autoplay: false
+	preload: false,autoplay: false
 	});
 var mySound2 = new buzz.sound("../../static/data/second_audio.ogg",
 	{
-	preload: true, autoplay:false
+	preload: false, autoplay:false
 	});
 var mySound3 = new buzz.sound("../../static/data/Second Audio Test.ogg",
 	{
-	preload: true, autoplay:false
+	preload: false, autoplay:false
 	});
+	
 //populate an array with all sounds we want to play
 mySounds.push(mySound1);
 mySounds.push(mySound2);
@@ -21,21 +25,60 @@ mySounds.push(mySound3);
 
 //load all sounds
 
-//for (var i=0;i<mySounds.length;i++)
-	//{
-	//mySounds[i].load();
-	//}
+for (var i=0;i<mySounds.length;i++)
+	{
+	mySounds[i].load();
+	}
+	
+//load the first sound
+mySounds[0].load();
+
+function playSound(current)
+	{
+	console.log("now playing " + (current+1));
+	mySounds[current].play();
+	current++;
+	if (mySounds.length > current)
+		{
+		mySounds[current].load();
+		playSound(current);
+		}
+	}
+
+playSound(0);
+
 //play all sounds to test they work
+/*
 for (var i=0;i<mySounds.length;i++)
 	{
 	mySounds[i].play();
 	}
-console.log(mySounds[0].getTime());
+*/
+//console.log(mySounds[0].getTime());
+//console.log(startTime);
+
+//stop all sounds
 
 for (var i=0;i<mySounds.length;i++)
 	{
 	mySounds[i].stop();
 	}
+
+
+
+
+
+
+
+
+
+
+
+//if (!mySounds[0].isEnded())
+//	{
+//	alert("the sound has not ended!");
+//	}
+
 ///////////////////// play factory ///////////////////
 //mySound.unmute();
 //mySound.setVolume(10);
