dbb28ff2a876e843ed645ad8603103379bfaafc5
U-PeterYordanov\Peter Yordanov <ppyordanov@yahoo.com>
2014-02-18 15:22:51 +0000

    fixed database settings which i broke acc.

--- mdrs/settings.py
+++ mdrs/settings.py
@@ -21,9 +21,9 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql', # Add 'postgresql_psycopg2', 'mysql', 'sqlite3' or 'oracle'.
         'NAME': 'mdrsDatabase',                      # Or path to database file if using sqlite3.
         # The following settings are not used with sqlite3:
-        'USER': 'root',
-        'PASSWORD': '',
-        'HOST': '127.0.0.1',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
+        'USER': 'mdrs',
+        'PASSWORD': 't34mt34mt',
+        'HOST': '',                      # Empty for localhost through domain sockets or '127.0.0.1' for localhost through TCP.
         'PORT': '',                      # Set to empty string for default.
     }
 }
