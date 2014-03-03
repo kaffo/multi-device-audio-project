878ee8fcc656b370d9c774de460163e4e9e82ac9
ppyordanov <ppyordanov@yahoo.com>
2014-03-03 14:12:26 +0000

    comments added to the file to explain structure

--- static/scripts/tl.js
+++ static/scripts/tl.js
@@ -1,11 +1,16 @@
-var selected = [];
-var all = false
+//pyordanov v2
 
+var selected = []; //an array of file ids to be synchronised
+var all = false //check the current status of the select all checkbox
+
+
+//function to add the selected file ids to the array
 function selectRec(){
 	
-	var rows = document.getElementsByName('rec');
-	selected = [];
+	var rows = document.getElementsByName('rec'); //get the div with id rec
+	selected = []; // empty the array
 	
+	//if the select all checkbox is marked, add all recordings to the array
 	if(rows[0].checked && !all){
 		for (var i = 1, l = rows.length; i < l; i++) {
 			selected.push(rows[i].id);
@@ -16,6 +21,7 @@ function selectRec(){
 		//alert(selected.length);
 	}
 	
+	//if the select all checkbox is not marked, remove all recordings to the array
 	else if(!rows[0].checked && all){
 	
 		selected = [];
@@ -28,6 +34,7 @@ function selectRec(){
 		alert("No recordings are selected.");
 	}
 	
+	//else check the selected check boxes and fill the array
 	else{
 		for (var i = 1, l = rows.length; i < l; i++) {
 			if (rows[i].checked) {
@@ -38,10 +45,11 @@ function selectRec(){
 	}
 	
 	//alert(selected.length);
-	syncSide(selected);
+	syncSide(selected); //synchronize the recordings
 	
 }
 
+//a function to populate the right sidebar with user's recordings
 function text(recs){
 
 	var Rdata="";
@@ -55,15 +63,20 @@ function text(recs){
 		c_desc = c_rec.fields.description;
 		c_file = c_rec.fields.rec_file;
 		
+		//recording id checkbox
 		Rdata += "<div class='Uinfo'><input type='checkbox' name='rec' id='" + c_id + "' onclick='selectRec();'/>"
+		//single recording playback
 		Rdata += "<div class='comp'><audio controls src='../../static/data/" + c_file + "'>Your user agent does not support the HTML5 Audio element.</audio><br><br>";
+		//download button
 		Rdata += "<form target='_blank' action='../../static/data/" + c_file + "'><input type='submit' value='Download' class='blue button'></form></div>"
 		//Rdata += "<div style='float:left;'><button class='blue button' onclick='this.firstChild.play()'><audio src='../../static/data/" + c_file + "'></audio>Play</button>";
+		//recording metadata - id, name, description
 		Rdata += "<p><b>ID:</b> " + c_id + "<br><b>Recording:</b> " + c_name + "<br><br><b>Description:</b><br>"  + c_desc + "</p>";
 		
 		Rdata += "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged.</div>";
 	}
 	
+	//populate the div with id uRecs with the relevant html code
 	document.getElementById("uRecs").innerHTML=Rdata;
 	
 }
@@ -72,6 +85,7 @@ function syncSide(selected){
 	syncArray(selected);
 }
 
+//auto resize code for the timeline and right sidebar
 function autoResizeDiv()
 {
 
@@ -88,7 +102,7 @@ function autoResizeDiv()
 	document.getElementById('info').style.height = Iheight;
 }
 
-
+//timeline initialization function
 $(document).ready(function() {
 	createStoryJS({
 		type:       'timeline',
